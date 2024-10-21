44597fa94b49f341da60d1b2e7c7835eceb5bbc1
Hatef <hatef.1382@gmail.com>
2024-10-21 10:52:50 +0330

    IF reg add

--- IF_Reg.v
+++ IF_Reg.v
@@ -1,5 +1,9 @@
 module IF_Reg(  input clk, rst, freeze, flush,
                 input [31:0] pcIn, instructionIn,
                 output reg [31:0] pc, Instruction);
+    // module register(input clk, rst ,loaden ,input[31:0] pin , output [31:0]pout );
+    register PCReg(clk , rst , ~freeze , (flush)? pcIn:32'd0 , pc);
+    register INSReg(clk , rst , ~freeze , (flush)? instructionIn:32'd0 , Instruction);
 
+    
 endmodule
