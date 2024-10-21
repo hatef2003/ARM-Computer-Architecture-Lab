0a5193dbe928f10e40504a7eddf5528856a2b231
Hatef <hatef.1382@gmail.com>
2024-10-21 10:19:28 +0330

    register added

---
+++ Register.v
@@ -0,0 +1,12 @@
+module register(input clk, rst ,loaden ,input[31:0] pin , output [31:0]pout );
+    reg [31:0] regVal;
+    always @(posedge clk ,posedge rst)
+    begin
+        if(rst)
+            regVal <= 32'd0;
+        if(loaden)
+            regVal  <= pin;
+
+    end
+    assign pout = regVal;
+endmodule
