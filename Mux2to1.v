795e2f3a84b56148759af4b248e98ea54f4942e7
ArianFiroozi <arianfiroozi3@gmail.com>
2024-10-21 14:02:45 +0330

    add: mux2to1

--- Mux2to1.v
+++ Mux2to1.v
@@ -1,1 +1,6 @@
-
+module Mux2to1( input select, 
+                [31:0] in0, 
+                [31:0] in1,
+                output [31:0] out)
+    assign output= (select==1'b1)?in1:in0;
+endmodule
