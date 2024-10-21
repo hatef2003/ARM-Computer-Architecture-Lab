cd7b8f726fecc0934480a3c27bc1ce1f6e3d5527
Hatef <hatef.1382@gmail.com>
2024-10-21 10:07:45 +0330

    pc added

--- ProgramCounter.v
+++ ProgramCounter.v
@@ -1,1 +1,11 @@
-
+module ProgramCounter(input clk  , branchTaken,freeze ,input[31:0]branchAddress , output  instructionAddress);
+    reg [31:0] counter ;
+    always @(posedge clk)
+    begin
+      if(branchTaken)
+        counter <= branchAddress;
+      else
+        counter <=counter + 31'd4;
+    end
+    assign instructionAddress = counter;
+endmodule
