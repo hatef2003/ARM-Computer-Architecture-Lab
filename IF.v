b977948e164f3942189f95a5690b428ea3efcbca
ArianFiroozi <arianfiroozi3@gmail.com>
2024-10-21 14:29:26 +0330

    update: wired instmem to if

--- IF.v
+++ IF.v
@@ -1,8 +1,12 @@
-module IF (input clk , rst , branchTaken ,freeze ,input[31:0] branchAdrress ,output [31:0] PC,instruction );
-    wire [31:0] PCIn , PCOut;
+module IF ( input clk , rst, branchTaken, freeze,
+            input[31:0] branchAdrress,
+            output [31:0] PC, instruction);
+
+    wire [31:0] PCIn, PCOut;
 
     Mux2to1 mux (branchTaken, PC, branchAddress, PCIn);
-    register pc(clk , rst , ~freeze, PCIn, PCOut );
-    ProgramCounter programAdder(PCOut , PC);    
+    register pc(clk , rst, ~freeze, PCIn, PCOut);
+    ProgramCounter programCounter(PCOut, PC);    
+    InstructionMemory instructionMem(PCOut, instruction)
 
 endmodule;
