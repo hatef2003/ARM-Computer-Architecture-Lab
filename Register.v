3a7fceec4539835b94ce2cbc1d35dacf00cedcb6
Hatef <hatef.1382@gmail.com>
2024-11-11 15:05:19 +0330

    changed: parametrizing demi register

--- Register.v
+++ Register.v
@@ -1,12 +1,16 @@
-module register(input clk, rst ,loaden ,input[31:0] pin , output [31:0]pout );
-    reg [31:0] regVal;
-    always @(posedge clk ,posedge rst)
-    begin
-        if(rst)
-            regVal <= 32'd0;
-        else if(loaden)
-            regVal  <= pin;
-
+module register #(parameter WIDTH = 32) (
+    input clk, 
+    input rst, 
+    input loaden, 
+    input [WIDTH-1:0] pin, 
+    output [WIDTH-1:0] pout
+);
+    reg [WIDTH-1:0] regVal;
+    always @(posedge clk or posedge rst) begin
+        if (rst) 
+            regVal <= {WIDTH{1'b0}};
+        else if (loaden)
+            regVal <= pin;
     end
     assign pout = regVal;
 endmodule
