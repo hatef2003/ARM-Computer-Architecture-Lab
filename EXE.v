b1966f89d7b998a5b0bec53e5d9d064968063853
ArianFiroozi <arianfiroozi3@gmail.com>
2024-11-11 14:36:34 +0330

    add: exe module complete

--- EXE.v
+++ EXE.v
@@ -1,5 +1,5 @@
 module EXE(input clk, rest, MEM_R_EN, MEM_W_EN, 
-            input EXE_CMD, B, S,
+            input[3:0] EXE_CMD,
             input [31:0] PC,
             input [31:0] val1, Rm, 
             input imm,
@@ -7,8 +7,21 @@ module EXE(input clk, rest, MEM_R_EN, MEM_W_EN,
             input[23:0] signedIMM, 
             input aluCarryOut,
             output memREnOut, memWEnOut,
-            output[31:0] ALURes, valRmOut, branchAddress);
+            output[31:0] ALURes, valRmOut, branchAddress,
+            output statusBits);
 
+    wire[31:0]val2_gen_out;
+    wire memCommand;
 
+    assign memREnOut=MEM_R_EN;
+    assign memWEnOut=MEM_W_EN;
+    assign valRmOut=Rm;
+    assign memCommand=MEM_R_EN|MEM_W_EN;
+
+    // adder
+    assign branchAddress=PC+{8{signedIMM[23]}, signedIMM}
+
+    ALU alu(val1, val2_gen_out, EXE_CMD, aluCarryOut, ALURes, statusBits);
+    ValGenerator val2generator(Rm, imm, memCommand, shiftOperand, val2_gen_out);
 
 endmodule
