03e5d5d1827f4d129cd9aa8b455817e0d52b23a2
ArianFiroozi <arianfiroozi3@gmail.com>
2024-12-22 12:54:17 +0330

    refactor

--- StatusReg.v
+++ StatusReg.v
@@ -1,13 +1,9 @@
 
-module StatusReg(clk, rst, en, statIn, statOut);
-
-    input [0:0] clk, rst, en;
-    input [3:0] statIn;
-
-    output reg[3:0] statOut;
-
-
-    always@(negedge clk or posedge rst) begin
+module StatusReg(input clk, rst, en, 
+                input [3:0] statIn, 
+                output reg [3:0] statOut);
+    always @(negedge clk or posedge rst) 
+    begin
         if(rst) 
             statOut <= 4'b0;
         else if(en) 
