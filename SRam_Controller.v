61971f68c1ceaab877732eaa372643b7a4fc4d99
ArianFiroozi <arianfiroozi3@gmail.com>
2024-12-23 10:34:41 +0330

    commit befor pull

--- SRam_Controller.v
+++ SRam_Controller.v
@@ -11,9 +11,10 @@ module SramController(input clk, rst, wr_en, rd_en,
     // sram_we=1, read high
     //
     // ready=1
-    wire[31:0] address_1024;
-    assign address_1024 = address-32'd1024;
-
+    wire[31:0] addr , temp;
+    assign temp = ALU_Res-32'd1024;
+      
+    assign addr={2'b00 , temp[31:2]};
     reg [2:0] ps, ns;
 
     always @(posedge clk) ps<=ns;
@@ -33,12 +34,27 @@ module SramController(input clk, rst, wr_en, rd_en,
     begin
         case (ps)
         3'b000:
-        3'b001:
+        3'b001: 
+        begin
+            assign ready = 1;
+            assign SRAM_WE_N=(wr_en==1'b1)? 1'b0 : (rd_en==1'b1)?1'b1 : SRAM_WE_N;
+        end
         3'b010:
+        begin
+            if (wr_en==1'b1) begin
+                
+            end
+            else begin
+                SRAM_ADDR = sramLowAddr;
+                readDataOut[15:0] <= SRAM_DQInOut;
+            end
+        end
         3'b011:
         3'b100:
         3'b101: assign ready<=1'd1;
         endcase
     end
+
+    assign {SRAM_UB_N, SRAM_LB_N, SRAM_CE_N, SRAM_OE_N} = 4'd0;
 
 endmodule
